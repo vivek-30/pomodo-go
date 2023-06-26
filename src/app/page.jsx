8321575795578aca66ec09c9d17b6c9be8f899de
vivek singh <vikky30singh@gmail.com>
2023-06-26 15:55:26 +0530

    minor style changes for header + timer section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-// import AddTask from '@components/AddTask';
+import AddTask from '@components/AddTask';
 import styles from '@styles/page.module.css';
 
 const Page = () => {
@@ -14,10 +14,12 @@ const Page = () => {
           <p>25:17:00</p>
           <p className="text-truncate">Complete pomodoro app</p>
         </div>
+        <div className={styles.divider}></div>
         <div className={styles['task-info']}>
           <span>Rounds: 3/4</span>
           <span>Estimated Time: 160 mins</span>
         </div>
+        <div className={styles.divider}></div>
         <div className={styles.controls}>
           <span>
             <Image className="top-left-center" src="/assets/icons/more.svg" alt="more-options icon" height={50} width={50} />
@@ -30,50 +32,48 @@ const Page = () => {
           </span>
         </div>
       </section>
-      {/* <section>
-        <div className="tasks-container">
-          <AddTask />
-          <div className="available-tasks">
-            <div className="tasks-filter-container">
-              <span>All</span>
-              <span>Pending</span>
-              <span>Completed</span>
-            </div>
-            <div className="tasks">
-              <ul role="list">
-                <li>
-                  <div>
-                    <span>name</span>
-                    <span>Rounds 2/3</span>
-                  </div>
-                  <div>Task description.... <span>Read More</span></div>
-                </li>
-                <li>
-                  <div>
-                    <span>name</span>
-                    <span>Rounds 2/3</span>
-                  </div>
-                  <div>Task description.... <span>Read More</span></div>
-                </li>
-                <li>
-                  <div>
-                    <span>name</span>
-                    <span>Rounds 2/3</span>
-                  </div>
-                  <div>Task description.... <span>Read More</span></div>
-                </li>
-                <li>
-                  <div>
-                    <span>name</span>
-                    <span>Rounds 2/3</span>
-                  </div>
-                  <div>Task description.... <span>Read More</span></div>
-                </li>
-              </ul>
-            </div>
+      <section className='tasks-section'>
+        <AddTask />
+        <div className="available-tasks">
+          <div className="tasks-filter-container">
+            <span>All</span>
+            <span>Pending</span>
+            <span>Completed</span>
+          </div>
+          <div className="tasks">
+            <ul role="list">
+              <li>
+                <div>
+                  <span>name</span>
+                  <span>Rounds 2/3</span>
+                </div>
+                <div>Task description.... <span>Read More</span></div>
+              </li>
+              <li>
+                <div>
+                  <span>name</span>
+                  <span>Rounds 2/3</span>
+                </div>
+                <div>Task description.... <span>Read More</span></div>
+              </li>
+              <li>
+                <div>
+                  <span>name</span>
+                  <span>Rounds 2/3</span>
+                </div>
+                <div>Task description.... <span>Read More</span></div>
+              </li>
+              <li>
+                <div>
+                  <span>name</span>
+                  <span>Rounds 2/3</span>
+                </div>
+                <div>Task description.... <span>Read More</span></div>
+              </li>
+            </ul>
           </div>
         </div>
-      </section> */}
+      </section>
     </main>
   );
 }
