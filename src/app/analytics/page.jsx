bde9d5738156a607432043b5a1276386c83de8cd
vivek singh <vikky30singh@gmail.com>
2023-07-01 23:02:53 +0530

    initialised analytics, settings and about page

---
+++ src/app/analytics/page.jsx
@@ -0,0 +1,7 @@
+const Analytics = () => {
+  return (
+    <div>Analytics page</div>
+  );
+}
+
+export default Analytics;
---
+++ src/app/settings/page.jsx
@@ -0,0 +1,7 @@
+const Settings = () => {
+  return (
+    <div>Settings page</div>
+  );
+}
+
+export default Settings;
