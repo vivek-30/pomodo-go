1f937d8bc658e019558aff55d6e20e5071c9f9ba
vivek singh <vikky30singh@gmail.com>
2023-07-01 23:01:31 +0530

    completed clock functionality

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,34 @@
+'use client';
+
 import Image from 'next/image';
 import Tasks from '@components/Tasks';
 import AddTask from '@components/AddTask';
+import { useState, useEffect } from 'react';
 import styles from '@styles/page.module.css';
 
+import timer from '@/utilities/timer';
+import formatTime from '@utilities/formatTime';
+
 const Page = () => {
+  const initialTime = {
+    hours: 2,
+    minutes: 3,
+    seconds: 22
+  }
+  let savedTime = null;
+  const [time, setTime] = useState(initialTime);
+  const [isPaused, setIsPaused] = useState(true);
+
+  let timerID = null;
+
+  useEffect(() => {
+    if(isPaused) return;
+    timerID = setInterval(timer(time, setTime), 1000);
+    return () => clearInterval(timerID);
+  }, [time, isPaused]);
+
+  const currentTime = formatTime(time);
+
   return (
     <main className={`${styles['main-container']} flex-center`}>
       <section className={`${styles['timer-section']} flex-center`}>
@@ -12,7 +37,7 @@ const Page = () => {
           <span>Stay Focused</span>
         </div>
         <div className={styles.timer}>
-          <p>25:17:00</p>
+          <p>{currentTime}</p>
           <p className="text-truncate">Complete pomodoro app</p>
         </div>
         <div className={styles.divider}></div>
@@ -22,13 +47,33 @@ const Page = () => {
         </div>
         <div className={styles.divider}></div>
         <div className={styles.controls}>
-          <span>
-            <Image className="top-left-center" src="/assets/icons/alarm.svg" alt="clock icon" height={44} width={44} />
+          <span onClick={() => {
+            setIsPaused(true);
+            setTime(initialTime);
+          }}>
+            <Image className="top-left-center" src="/assets/icons/redo.svg" alt="reset icon" height={25} width={25} />
           </span>
-          <span>
-            <Image className="top-left-center" src="/assets/icons/play.svg" alt="play icon" height={50} width={50} />
+          <span onClick={() => {
+            savedTime = time;
+            setIsPaused(currentState => !currentState);
+
+            if(isPaused) {
+              clearInterval(timerID);
+            } else {
+              setTime(savedTime);
+            }
+          }}>
+            <Image 
+              className="top-left-center" 
+              src={`/assets/icons/${isPaused ? 'play' : 'pause'}.svg`}
+              alt={`${isPaused ? 'play' : 'pause'} icon`} 
+              height={60} 
+              width={60}
+            />
           </span>
-          <span>
+          <span onClick={() => {
+            setTime({ hourse: 0, minutes: 0, seconds: 0 });
+          }}>
             <Image className="top-left-center" src="/assets/icons/forward.svg" alt="foward icon" height={50} width={50} />
           </span>
         </div>
