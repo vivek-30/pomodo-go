b70a805b28069567873fd03e078ac0aee9b18bd3
vivek singh <vikky30singh@gmail.com>
2023-07-05 19:21:07 +0530

    created a utility method for managing timer's status info

---
+++ src/utilities/manageStatusInfo.js
@@ -0,0 +1,28 @@
+import Image from 'next/image';
+import { useContext } from 'react';
+import { ModeContext } from '@contexts/modeContext';
+
+const manageStatusInfo = () => {
+  const { state } = useContext(ModeContext);
+  const statusInfo = {
+    text: '',
+    image: null
+  };
+
+  if(state.mode === 'focus') {
+    statusInfo.text = 'Stay Focused';
+    statusInfo.image = <Image src="/assets/icons/dart.svg" alt="dart icon" width={35} height={35} />;
+  }
+  else if(state.mode === 'short-break') {
+    statusInfo.text = 'Short Break';
+    statusInfo.image = <Image src="/assets/icons/coffee.svg" alt="coffee-cup icon" width={35} height={35} />;
+  }
+  else {
+    statusInfo.text = 'Long Break';
+    statusInfo.image = <Image src="/assets/icons/gaming.svg" alt="joystick icon" width={35} height={35} />;
+  }
+
+  return statusInfo;
+}
+
+export default manageStatusInfo;
