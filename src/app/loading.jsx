8659037b33062d29e368ccef62a0fe646b961120
vivek singh <vikky30singh@gmail.com>
2023-07-19 00:07:28 +0530

    incorporated tailwindcss in loading.jsx

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -1,17 +1,14 @@
 'use client';
 import { useContext } from 'react';
 import { ModeContext } from '@contexts/modeContext';
-import styles from '@styles/pages/loading.module.scss';
 
 const Loading = () => {
-  const { state } = useContext(ModeContext);
+  const { state: { mode } } = useContext(ModeContext);
   
   return (
-    <section className={`${styles['loading__container']} flex justify-center items-center`}>
-      <div className={`${styles['loading__container-clock']} ${styles[state.mode]} before:top-2/4 before:left-2/4 before:-translate-x-2/4 before:-translate-y-2/4 after:top-2/4 after:left-2/4 after:-translate-x-2/4 after:-translate-y-2/4`}>
-        <span className="top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4"></span>
-      </div>
-      <p>Loading</p>
+    <section className="w-screen h-screen flex flex-col justify-center items-center">
+      <div className={`relative w-16 h-16 border-[0.4rem] border-[#333] rounded-full before:absolute before:content-[''] before:w-[0.3rem] before:h-6 before:origin-top before:top-2/4 before:left-2/4 before:animate-spin ${mode === 'focus' ? 'before:bg-red-800' : mode === 'short-break' ? 'before:bg-green-800' : 'before:bg-sky-800'}`} />
+      <p className="my-2 mx-auto text-2xl">Loading...</p>
     </section>
   );
 }
